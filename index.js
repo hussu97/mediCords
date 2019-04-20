61ff08d1bc3cce589dbd067de7f7e91475e9d7a9
Hussain Abbasi <H_abbasi97@hotmail.com>
2019-04-20 23:45:25 +0400

    bills page

--- index.js
+++ index.js
@@ -18,101 +18,17 @@ var indexRoutes = require('./routes/index');
 var moment = require('moment');
 port = 8080;
 
-app.use(flash());
+// app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 var urlencodedParser = bodyParser.urlencoded({
   extended: false
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
-
-app.get('/patient', (req, res) => {
-  res.render('patient/profile',{
-    patient : {
-      firstName : 'John',
-      lastName : 'Doe',
-      country : 'Madagascar',
-      isVerified : true,
-      id : 123
-    },
-    countryList : countryList
-  });
-});
-app.get('/patientOperation', (req, res) => {
-  res.render('patient/operation',{
-    patient : {
-      firstName : 'John',
-      lastName : 'Doe',
-      country : 'Madagascar',
-      isVerified : true,
-      id : 123
-    },
-    operations : [
-      {
-        date: moment().format('MMM Do YY'),
-        name: 'Tonsil Surgery',
-        doctor: 'Steven',
-        dischargeDate : moment().subtract(10, 'days').format('MMM Do YY'),
-        daysInHospital : '23',
-        comment:'Successful operation'
-      }
-    ]
-  });
-});
-app.get('/patientAllergy', (req, res) => {
-  res.render('patient/allergy',{
-    patient : {
-      firstName : 'John',
-      lastName : 'Doe',
-      country : 'Madagascar',
-      isVerified : true,
-      id : 123
-    },
-    allergies : [
-      {
-        name : 'Peanut Butter',
-        doctor : 'Hello',
-        date : moment().format('MMM Do YY'),
-        severity : 'low',
-        medication : 'N/A'
-      },
-      {
-        name : 'Peanut Butter',
-        doctor : 'Hello',
-        date : moment().format('MMM Do YY'),
-        severity : 'med',
-        medication : 'N/A'
-      },
-      {
-        name : 'Peanut Butter',
-        doctor : 'Hello',
-        date : moment().format('MMM Do YY'),
-        severity : 'high',
-        medication : 'Water'
-      }
-    ]
-  });
-});
-app.get('patient/:id/profile',(req,res) => {
-    res.render('patient/profile',{
-        patient : {
-          firstName : 'John',
-          lastName : 'Doe',
-          country : 'Madagascar',
-          isVerified : true,
-          id : req.params.id
-        },
-        countryList : countryList
-      });
-});
-app.put('patient/:id/profile', (req,res)=> {
-    res.redirect('/patient/'+req.params.id);
-});
-
-
 app.use('/',indexRoutes);
-//app.use('/patient',patientRoutes);
+app.use('/patient',patientRoutes);
 app.get('/2', (req, res) => {
   res.sendFile(path.join(__dirname + '/./cards.html'));
 });
@@ -127,7 +43,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/./404.html'));
 });
 
-app.set('view engine', 'ejs');
+
 
 app.listen(port, () => {
   console.log(`Server Starts on ${port}`);
--- routes/patients.js
+++ routes/patients.js
@@ -1,55 +1,223 @@
-const express    = require('express'),
-      router     = express.Router({
-        mergeParams: true
-      }),
-      app=express(),
-      path = require('path'),
-      {countryList} = require('../public/js/constants');
+const express = require('express'),
+  router = express.Router({
+    mergeParams: true
+  }),
+  path = require('path'),
+  {
+    countryList
+  } = require('../public/js/constants'),
+  moment = require('moment');
 
 
+//=========================================
+//                patient profile routes
+//=========================================
 router.get('/:id', (req, res) => {
-  res.render('profile',{
-    patient : {
-      firstName : 'John',
-      lastName : 'Doe',
-      country : 'Madagascar',
-      isVerified : true,
-      id : req.params.id
+  res.render('patient/profile', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
     },
-    countryList : countryList
+    countryList: countryList
   });
 });
-router.get('/:id/profile',(req,res) => {
-    res.render('patient/profile',{
-        patient : {
-          firstName : 'John',
-          lastName : 'Doe',
-          country : 'Madagascar',
-          isVerified : true,
-          id : req.params.id
-        },
-        countryList : countryList
-      });
-});
-router.put('/:id/profile', (req,res)=> {
-    res.redirect('/patient/'+req.params.id);
-});
-router.get('/:id/operation',(req,res) => {
-    res.render('patient/operation',{
-        patient : {
-            firstName : 'John',
-            lastName : 'Doe',
-            country : 'Madagascar',
-            isVerified : true,
-            id : req.params.id
-          },
-        operation : {
-
-        }
-    });
-});
-
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../404.html'));
+router.get('/:id/profile', (req, res) => {
+  res.render('patient/profile', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
+    },
+    countryList: countryList
   });
+});
+//update patient profile
+router.put('/:id/profile', (req, res) => {
+  res.redirect('/patient/' + req.params.id);
+});
+//=========================================
+//                patient medical info routes
+//=========================================
+router.get('/:id/operation', (req, res) => {
+  res.render('patient/operation', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
+    },
+    operations: [{
+      date: moment().format('MMM Do YY'),
+      name: 'Tonsil Surgery',
+      doctor: 'Steven',
+      dischargeDate: moment().subtract(10, 'days').format('MMM Do YY'),
+      daysInHospital: '23',
+      comment: 'Successful operation'
+    }]
+  });
+});
+router.get('/:id/allergy', (req, res) => {
+  res.render('patient/allergy', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
+    },
+    allergies: [{
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'low',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'med',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'high',
+        medication: 'Water'
+      }
+    ]
+  });
+});
+router.get('/:id/disease', (req, res) => {
+  res.render('patient/allergy', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
+    },
+    allergies: [{
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'low',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'med',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'high',
+        medication: 'Water'
+      }
+    ]
+  });
+});
+router.get('/:id/disability', (req, res) => {
+  res.render('patient/allergy', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
+    },
+    allergies: [{
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'low',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'med',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'high',
+        medication: 'Water'
+      }
+    ]
+  });
+});
+//=========================================
+//                patient bills routes
+//=========================================
+router.get('/:id/bill', (req, res) => {
+  res.render('patient/bill', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
+    },
+    bills: [{
+      id: '1234',
+      date: moment().format('MMM Do YY'),
+      hospital: 'XYZ Hospital',
+      doctor: 'Mazin Raf',
+      cost: 234.34,
+      vat: 5.67,
+      total : 245.23
+    }]
+  });
+});
+//=========================================
+//                patient insurance routes
+//=========================================
+router.get('/:id/insurance', (req, res) => {
+  res.render('patient/allergy', {
+    patient: {
+      firstName: 'John',
+      lastName: 'Doe',
+      country: 'Madagascar',
+      isVerified: true,
+      id: req.params.id
+    },
+    allergies: [{
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'low',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'med',
+        medication: 'N/A'
+      },
+      {
+        name: 'Peanut Butter',
+        doctor: 'Hello',
+        date: moment().format('MMM Do YY'),
+        severity: 'high',
+        medication: 'Water'
+      }
+    ]
+  });
+});
 module.exports = router;
